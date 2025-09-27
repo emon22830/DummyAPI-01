7fa7e8bd7ad0c2f7e2ee73c9a4b95562fca4d8c4
emon22830 <mdemon22830@gmail.com>
2025-09-27 13:18:36 +0800

    f

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,7 @@
 /* A reusable card component */
 export default function Card({ title, endpoint }) {
   const handleClick = () => {
-    window.open(`https://dummy-api-01.vercel.app/${endpoint}`, "_blank");
+    window.open(`https://dummy-api-01-8ant.vercel.app/${endpoint}`, "_blank");
   };
 
   return (
